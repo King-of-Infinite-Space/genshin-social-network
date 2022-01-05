eaa22072214e8eeddbaac24db35bcd02163ad981
King-of-Infinite-Space <27502578+King-of-Infinite-Space@users.noreply.github.com>
2022-01-04 20:55:13 -0500

    fix workflow script author

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -1,14 +1,19 @@
 MSG=$(cat msg.log)
 if [[ -z "$MSG" ]]; then
     echo "No changes to commit"
-elif [[ "$MSG" == *"updating" ]]; then
+    exit
+fi
+
+git config user.name "github-actions[bot]"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+if [[ "$MSG" == *"updating" ]]; then
     git restore utils/char_list.json
-    git commit -a -m "$MSG" --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>"
+    git commit -a -m "$MSG"
     git push
     echo "Committed partial update"
     exit 1
 else
-    git commit -a -m "$MSG" --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>"
+    git commit -a -m "$MSG"
     git push
     echo "Committed full update"
 fi
